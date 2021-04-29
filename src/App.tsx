afdad1a9b5fcfaded965c1fcc741b028bd10f708
victor chao <foureyeschao@gmail.com>
2021-04-29 11:56:29 -0400

    suspend

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,47 @@
 import React from 'react';
+<<<<<<< Updated upstream
 import logo from './logo.svg';
 import styles from './App.module.css';
+=======
+import styles from './App.module.css'
+import { Header, Footer, SideMenu, Carousel } from './components'
+import { Row, Col, Typography } from 'antd'
+
+import { productList1, productList2, productList3 } from './mockups'
+import sideImage1 from './assets/images/sider_2019_12-09.png'
+import sideImage2 from './assets/images/sider_2019_02-04.png'
+import sideImage3 from './assets/images/sider_2019_02-04-2.png'
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <div className={styles.App}>
+<<<<<<< Updated upstream
       <header className={styles['App-header']}>
         <img src={logo} className={styles['App-logo']} alt="logo" />
         <p>
           React-Travel is initialized.
         </p>
       </header>
+=======
+      <Header />
+      <div className={styles['page-content']}>
+        <Row style={{ marginTop: 20 }}>
+          <Col span={6}><SideMenu /></Col>
+          <Col span={18}><Carousel /></Col>
+        </Row>
+        <ProductCollection
+          title={
+            <Typography.Title level={3} type="warning">
+              Best Deals
+            </Typography.Title>
+          }
+          sideImage={sideImage1}
+          products={productList1}
+        />
+      </div>
+      <Footer />
+>>>>>>> Stashed changes
     </div>
   );
 }
---
+++ src/components/productCollection/ProductCollection.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './ProductCollection.module.css'
+
+export const ProductCollection: React.FC = () => {
+  return (
+    <div></div>
+  )
+}
